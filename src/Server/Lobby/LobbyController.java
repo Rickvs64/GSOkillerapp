f1661891f23b9e3cc53caae8ea9a0081dd3fe8c3
Rick <rick.van.schijndel@hotmail.com>
2018-01-12 16:00:59 +0100

    Added lobby screen

--- src/Server/Lobby/LobbyController.java
+++ src/Server/Lobby/LobbyController.java
@@ -1,22 +1,65 @@
 package Server.Lobby;
 
+import Classes.ILobby;
 import Classes.Player;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LobbyController {
-    private Player player;
+    private Player player1;
+    private Player player2;
+    private ILobby lobby;
 
     @FXML
-    private Label lbl_username;
+    private Label lbl_status;
+    @FXML
+    private Label lbl_lobbyname;
+    @FXML
+    private TextField txt_lobbyname;
+    @FXML
+    private Label lbl_lobbynameInput;
+    @FXML
+    private Button btn_lobbynameSubmit;
+    @FXML
+    private Label lbl_player1;
+    @FXML
+    private Label lbl_player2;
+    @FXML
+    private Label lbl_player1DIST;
+    @FXML
+    private Label lbl_player2DIST;
+    @FXML
+    private Label lbl_player1STAT;
+    @FXML
+    private Label lbl_player2STAT;
+    @FXML
+    private Button btn_start;
 
 
-    private void updateUserInfo() {
-        lbl_username.setText(player.getUsername());
+    public LobbyController() {
+        // Initialize the UI.
+        initUI();
     }
 
-    public void initialize(Player player) {
-        this.player = player;
-        updateUserInfo();
+    public void initUI() {
+        Platform.runLater(new Runnable() {
+            @Override public void run() {
+                btn_start.setVisible(false);
+            }
+        });
+    }
+
+
+    @FXML
+    public void LobbyNameSubmit(ActionEvent actionEvent) {
+        System.out.println("Hoi");
+    }
+
+    @FXML
+    public void StartGame(ActionEvent actionEvent) {
     }
 }
